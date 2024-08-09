e6bb08fb160e91a682556821fa1a081b017193e9
Kyle <kyletupling@hotmail.com>
2024-08-09 13:07:49 +0100

    Updated default text color to white

--- kEngine/UIButton.cpp
+++ kEngine/UIButton.cpp
@@ -16,7 +16,7 @@ void UIButton::Draw(SDL_Renderer* renderer)
 	
 	SDL_RenderFillRect(renderer, &rect);
 
-	SDL_Surface* surface = TTF_RenderText_Blended(font, text.c_str(), { 255, 0, 0, 255 });
+	SDL_Surface* surface = TTF_RenderText_Blended(font, text.c_str(), { 255, 255, 255, 255 });
 	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
 	SDL_Rect textRect = { rect.x + rect.w / 2 - surface->w / 2, rect.y + rect.h / 2 - surface->h / 2, surface->w, surface->h };
 	SDL_RenderCopy(renderer, texture, NULL, &textRect);
