89d7aa8fcde259e067aa0d46d95eefab53742cff
Kyle <kyletupling@hotmail.com>
2024-08-09 12:31:34 +0100

    Separated SDL init to function

--- kEngine/main.cpp
+++ kEngine/main.cpp
@@ -25,6 +25,23 @@ SDL_Color colorWhite = { 255, 255, 255, 255 };
 SDL_Color colorBlack = { 0, 0, 0, 255 };
 SDL_Color colorGrey = { 100, 100, 100, 255 };
 
+bool InitializeSDL()
+{
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0)
+    {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+    if (TTF_Init() != 0)
+    {
+        std::cerr << "TTF_Init Error: " << TTF_GetError() << std::endl;
+        SDL_Quit();
+        return false;
+    }
+    SDL_ShowCursor(SDL_DISABLE);
+    return true;
+}
+
 // Converts given variable to a std::string
 template <typename T>
 std::string to_string(T x)
@@ -122,9 +139,10 @@ int main(int argv, char** args)
     body2.mass = 1e18;
 
     // Initialise SDL
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
-    TTF_Init();
-    SDL_ShowCursor(SDL_DISABLE);
+    if (!InitializeSDL())
+    {
+        return 1;
+    }
 
     // TODO: Create Engine class to manage SDL window, renderer etc.
     SDL_Window* window = SDL_CreateWindow("kEngine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenSize.x, screenSize.y, SDL_WINDOW_OPENGL);
