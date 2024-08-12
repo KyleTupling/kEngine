66f77ea85d52b0a8790904dafe553344c42a4894
Kyle <kyletupling@hotmail.com>
2024-08-12 14:05:28 +0100

    Added protected isFixedToScreen member

--- kEngine/UIElement.h
+++ kEngine/UIElement.h
@@ -1,12 +1,21 @@
 #pragma once
 
 #include <SDL.h>
+#include "Camera.h"
+#include "Vector2D.h"
 
 class UIElement
 {
 public:
-	virtual void Draw(SDL_Renderer* renderer) = 0;
+	virtual void Draw(SDL_Renderer* renderer, const Camera& camera, const Vector2D& screenSize) = 0;
 	virtual void HandleEvent(const SDL_Event& event) = 0;
 	virtual ~UIElement() {}
+
+	bool GetIsFixedToScreen() const { return m_isFixedToScreen; }
+
+	void SetIsFixedToScreen(bool isFixed) { m_isFixedToScreen = isFixed; }
+
+protected:
+	bool m_isFixedToScreen = true;
 };
 
