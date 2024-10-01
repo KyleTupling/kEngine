2bb7aea92b29fa2494aa3562e0e4d289e48cf862
Kyle <kyletupling@hotmail.com>
2024-10-01 12:04:48 +0100

    Created file, added base colors

---
+++ kEngine/Graphics/Color.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <SDL.h>
+
+namespace Color
+{
+	constexpr SDL_Color White = { 255, 255, 255, 255 };
+	constexpr SDL_Color Black = { 0, 0, 0, 255 };
+	constexpr SDL_Color Grey = { 100, 100, 100, 255 };
+}
