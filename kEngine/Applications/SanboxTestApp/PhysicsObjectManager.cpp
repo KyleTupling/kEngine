1b43915af22aa22c2e4b484a0dc591a7f11004a4
Kyle <kyletupling@hotmail.com>
2024-11-06 19:32:13 +0000

    Created PhysicsObjectManager

---
+++ kEngine/Applications/SanboxTestApp/PhysicsObjectManager.cpp
@@ -0,0 +1,34 @@
+#include "PhysicsObjectManager.h"
+
+PhysicsObjectManager::PhysicsObjectManager()
+{
+}
+
+void PhysicsObjectManager::Update()
+{
+	for (auto obj : m_PhysicsObjectsToAdd)
+	{
+		m_PhysicsObjects.push_back(obj);
+	}
+	for (auto obj : m_PhysicsObjects)
+	{
+		if (obj->ShouldBeRemoved())
+		{
+			// Remove PhysicsObject from vector
+		}
+	}
+	// Clear object-add buffer
+	m_PhysicsObjectsToAdd.clear();
+}
+
+std::shared_ptr<PhysicsObject> PhysicsObjectManager::AddPhysicsObject(PhysicsType type, const Vector2D& position)
+{
+	auto object = std::make_shared<PhysicsObject>(type, position);
+	m_PhysicsObjectsToAdd.push_back(object);
+	return object;
+}
+
+PhysicsObjectVector& PhysicsObjectManager::GetPhysicsObjects() const
+{
+	// TODO: insert return statement here
+}
---
+++ kEngine/Applications/SanboxTestApp/PhysicsObjectManager.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "PhysicsObject.h"
+#include "Vector2D.h"
+
+#include <memory>
+#include <vector>
+
+typedef std::vector<std::shared_ptr<PhysicsObject>> PhysicsObjectVector;
+
+class PhysicsObjectManager
+{
+public:
+	PhysicsObjectManager();
+
+	/**
+	 * Updates the vector of PhysicsObjects with objects in deferred vector.
+	 */
+	void Update();
+
+	std::shared_ptr<PhysicsObject> AddPhysicsObject(PhysicsType type, const Vector2D& position);
+
+	PhysicsObjectVector& GetPhysicsObjects() const;
+private:
+	PhysicsObjectVector m_PhysicsObjects;
+	PhysicsObjectVector m_PhysicsObjectsToAdd;
+	size_t m_TotalPhysicsObjects;
+};
+
