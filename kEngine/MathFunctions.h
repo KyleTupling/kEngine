08289a2567df6fae126f0a2995a6ba0eb6070ade
Kyle <kyletupling@hotmail.com>
2024-11-06 19:59:16 +0000

    Added lerp functions

--- kEngine/MathFunctions.h
+++ kEngine/MathFunctions.h
@@ -7,4 +7,17 @@ namespace MathFunctions
 	{
 		return std::max(lower, std::min(num, upper));
 	}
+
+	template <typename T>
+	T Lerp(const T& current, const T& target, const T& t)
+	{
+		return current * (1.0f - t) + target * t;
+	}
+
+	template <typename T>
+	T ExpLerp(const T& current, const T& target, const T& t)
+	{
+		float decayFactor = 1.0f - std::exp(-t);
+		return current + (target - current) * decayFactor;
+	}
 }
